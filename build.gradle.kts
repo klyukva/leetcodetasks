5c6dbb4a596e01694d7c7357ddef47fe079c1335
Grigoriy <bogatovg@gmail.com>
2025-05-23 23:38:18 +0300

    problem-49: group anagrams (#25)

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.12.2"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.12.2")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.12.2")
+    testImplementation("org.hamcrest:hamcrest:3.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation(kotlin("stdlib-jdk8"))
 }
