1c8e337d6abbe5e816542ede5b4a0a635735060d
grigorii <bogatovg@gmail.com>
2025-05-23 20:30:39 +0300

    problem-1701: avg waiting time

---
+++ src/test/java/com/leetcode/problems_1701_1800/problem_1701/Solution1701Test.kt
@@ -0,0 +1,27 @@
+package com.leetcode.problems_1701_1800.problem_1701
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class Solution1701Test {
+    private val solution1701 = Solution1701()
+
+    @ParameterizedTest
+    @MethodSource("arrayProvider")
+    fun `examples avg waiting time`(customers: Array<IntArray>, expected: Double) {
+        val result = solution1701.averageWaitingTime(customers)
+        assertEquals(expected, result, 0.00001)
+    }
+
+    companion object {
+
+        @JvmStatic
+        private fun arrayProvider() =
+            listOf(
+                Arguments.of(arrayOf(intArrayOf(1,2), intArrayOf(2,5), intArrayOf(4,3)), 5.0),
+                Arguments.of(arrayOf(intArrayOf(5,2), intArrayOf(5,4), intArrayOf(10,3), intArrayOf(20,1)), 3.25),
+            )
+    }
+}
